b2f494a9ea0db520b6b3166d8ef7ded83457d62c
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-12-07 17:37:43 +0100

    Added first name and last name to user

--- src/main/java/pl/poznan/put/bsr/bank/models/User.java
+++ src/main/java/pl/poznan/put/bsr/bank/models/User.java
@@ -24,13 +24,62 @@ public class User {
     private String userName;
     @NotNull
     private byte[] password;
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String lastName;
     @Reference
     private List<BankAccount> bankAccounts;
 
-    public User(String userName, String password) {
+    public User() {
+    }
+
+    public User(String userName, String password, String firstName, String lastName) {
         this.userName = userName;
         this.password = Base64.getEncoder().encode(password.getBytes());
+        this.firstName = firstName;
+        this.lastName = lastName;
         bankAccounts = new ArrayList<>();
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public byte[] getPassword() {
+        return password;
+    }
+
+    public void setPassword(byte[] password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public List<BankAccount> getBankAccounts() {
