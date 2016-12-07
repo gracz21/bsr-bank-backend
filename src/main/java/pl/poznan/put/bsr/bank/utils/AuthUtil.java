9ccab49219a9e96c1a75afea850507c2512ab8bc
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-12-07 19:39:20 +0100

    Added authentication util class

---
+++ src/main/java/pl/poznan/put/bsr/bank/utils/AuthUtil.java
@@ -0,0 +1,35 @@
+package pl.poznan.put.bsr.bank.utils;
+
+import org.mongodb.morphia.Datastore;
+import pl.poznan.put.bsr.bank.models.Session;
+import pl.poznan.put.bsr.bank.services.exceptions.BankServiceException;
+
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * @author Kamil Walkowiak
+ */
+public class AuthUtil {
+    public static String getSessionIdFromHeaders(WebServiceContext context) throws BankServiceException {
+        Map headers = (Map)context.getMessageContext().get(MessageContext.HTTP_REQUEST_HEADERS);
+        ArrayList sessionId = (ArrayList)headers.get("Session-Id");
+        if(sessionId != null) {
+            return (String)sessionId.get(0);
+        } else {
+            throw new BankServiceException("Session id is missing");
+        }
+    }
+
+    public static Session getSessionObject(String sessionId) throws BankServiceException {
+        Datastore datastore =  DataStoreHandlerUtil.getInstance().getDataStore();
+        Session session = datastore.find(Session.class).field("sessionId").equal(sessionId).get();
+        if(session != null) {
+            return session;
+        } else {
+            throw new BankServiceException("User is not logged in or session has expired");
+        }
+    }
+}
