f22f1f6fc77b506b1bd4749f77035f7421153984
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-12-07 17:38:59 +0100

    Finished register method, added login method

--- src/main/java/pl/poznan/put/bsr/bank/services/UserService.java
+++ src/main/java/pl/poznan/put/bsr/bank/services/UserService.java
@@ -1,13 +1,18 @@
 package pl.poznan.put.bsr.bank.services;
 
+import com.mongodb.DuplicateKeyException;
 import org.mongodb.morphia.Datastore;
 import pl.poznan.put.bsr.bank.models.User;
-import pl.poznan.put.bsr.bank.services.messages.RegisterMessage;
+import pl.poznan.put.bsr.bank.services.exceptions.BankServiceException;
 import pl.poznan.put.bsr.bank.utils.DataStoreHandlerUtil;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
+import javax.xml.bind.annotation.XmlElement;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.UUID;
 
 /**
  * @author Kamil Walkowiak
@@ -15,8 +20,31 @@ import javax.jws.WebService;
 @WebService
 public class UserService {
     @WebMethod
-    public void register(@WebParam RegisterMessage registerMessage) {
+    public void register(@WebParam(name = "userName") @XmlElement(required=true) String userName,
+                         @WebParam(name = "password") @XmlElement(required=true) String password,
+                         @WebParam(name = "firstName") @XmlElement(required=true) String firstName,
+                         @WebParam(name = "lastName") @XmlElement(required=true) String lastName)
+            throws BankServiceException {
         Datastore datastore =  DataStoreHandlerUtil.getInstance().getDataStore();
-        datastore.save(new User(registerMessage.getUserName(), registerMessage.getPassword()));
+        try {
+            datastore.save(new User(userName, password, firstName, lastName));
+        } catch(DuplicateKeyException exception) {
+            throw new BankServiceException("User name already used");
+        }
+    }
+
+    @WebMethod
+    public String login(@WebParam(name = "userName") @XmlElement(required=true) String userName,
+                      @WebParam(name = "password") @XmlElement(required=true) String password)
+            throws BankServiceException {
+        Datastore datastore =  DataStoreHandlerUtil.getInstance().getDataStore();
+        User user = datastore.find(User.class).field("userName").equal(userName).get();
+        if(user == null) {
+            throw new BankServiceException("Wrong user name");
+        } else if(Arrays.equals(user.getPassword(), Base64.getEncoder().encode(password.getBytes()))) {
+            return UUID.randomUUID().toString();
+        } else {
+            throw new BankServiceException("Wrong password");
+        }
     }
 }
