519fa20f06a4aa6512af0513ac4fb7fdd92ea7a8
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-12-07 17:38:32 +0100

    Added bank service exception class

---
+++ src/main/java/pl/poznan/put/bsr/bank/services/exceptions/BankServiceException.java
@@ -0,0 +1,15 @@
+package pl.poznan.put.bsr.bank.services.exceptions;
+
+/**
+ * @author Kamil Walkowiak
+ */
+public class BankServiceException extends Exception {
+    public BankServiceException() {
+    }
+
+    public BankServiceException(String message) {
+        super(message);
+    }
+
+
+}
