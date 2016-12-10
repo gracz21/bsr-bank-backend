444e422a9f3cc898a473274586e4ae12420a97ef
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-12-10 19:16:57 +0100

    Implemented withdraw bank operation

--- src/main/java/pl/poznan/put/bsr/bank/models/bankOperations/Withdrawal.java
+++ src/main/java/pl/poznan/put/bsr/bank/models/bankOperations/Withdrawal.java
@@ -1,6 +1,8 @@
 package pl.poznan.put.bsr.bank.models.bankOperations;
 
+import org.mongodb.morphia.Datastore;
 import pl.poznan.put.bsr.bank.exceptions.BankOperationException;
+import pl.poznan.put.bsr.bank.models.BankAccount;
 
 /**
  * @author Kamil Walkowiak
@@ -14,7 +16,12 @@ public class Withdrawal extends BankOperation {
     }
 
     @Override
-    public void doOperation() throws BankOperationException {
-        super.doOperation();
+    protected void execute(Datastore datastore) throws BankOperationException {
+        BankAccount bankAccount = datastore.find(BankAccount.class).field("accountNo").equal(targetAccountNo).get();
+        if(bankAccount == null) {
+            throw new BankOperationException("Target bank account does not exist");
+        }
+
+        bankAccount.setBalance(bankAccount.getBalance() - amount);
     }
 }
