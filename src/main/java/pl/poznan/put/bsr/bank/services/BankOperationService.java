da0253a307344fb69522d018d052507dbb374d40
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-12-11 23:31:23 +0100

    Implemented internal transfer method

--- src/main/java/pl/poznan/put/bsr/bank/services/BankOperationService.java
+++ src/main/java/pl/poznan/put/bsr/bank/services/BankOperationService.java
@@ -75,4 +75,52 @@ public class BankOperationService {
         withdrawal.doOperation(bankAccount);
         datastore.save(bankAccount);
     }
+
+    @WebMethod
+    public void makeTransfer(@WebParam(name = "title") @XmlElement(required = true) String title,
+                             @WebParam(name = "amount") @XmlElement(required = true) double amount,
+                             @WebParam(name = "sourceAccountNo") @XmlElement(required = true) String sourceAccountNo,
+                             @WebParam(name = "targetAccountNo") @XmlElement(required = true) String targetAccountNo)
+            throws BankServiceException, BankOperationException, ValidationException, AuthException {
+        SAXExceptionToValidationExceptionUtil.parseExceptions(context.getMessageContext());
+
+        Datastore datastore = DataStoreHandlerUtil.getInstance().getDataStore();
+        User user = AuthUtil.getUserFromWebServiceContext(context, datastore);
+
+        BankAccount sourceBankAccount = datastore.find(BankAccount.class).field("accountNo").equal(sourceAccountNo).get();
+        if(sourceBankAccount == null) {
+            throw new BankServiceException("Source bank account does not exist");
+        }
+        if(!user.containsBankAccount(sourceAccountNo)) {
+            throw new BankServiceException("Source account does not belong to user");
+        }
+
+        Transfer outTransfer = new Transfer(title, amount, sourceAccountNo, targetAccountNo, Transfer.TransferDirection.OUT);
+
+        if(targetAccountNo.substring(2, 10).equals(ConstantsUtil.BANK_ID)) {
+            BankAccount targetBankAccount = datastore.find(BankAccount.class).field("accountNo").equal(targetAccountNo).get();
+            if(targetBankAccount == null) {
+                throw new BankServiceException("Target bank account does not exist");
+            }
+            Transfer inTransfer = new Transfer(title, amount, sourceAccountNo, targetAccountNo, Transfer.TransferDirection.IN);
+            makeInternalTransfer(datastore, sourceBankAccount, targetBankAccount, inTransfer, outTransfer);
+        } else {
+            makeExternalTransfer(datastore, sourceBankAccount, outTransfer);
+        }
+    }
+
+    private void makeInternalTransfer(Datastore datastore, BankAccount sourceBankAccount, BankAccount targetBankAccount,
+                                      Transfer inTransfer, Transfer outTransfer)
+            throws BankOperationException {
+        outTransfer.doOperation(sourceBankAccount);
+        inTransfer.doOperation(targetBankAccount);
+
+        datastore.save(sourceBankAccount);
+        datastore.save(targetBankAccount);
+    }
+
+    private void makeExternalTransfer(Datastore datastore, BankAccount sourceBankAccount, Transfer outTransfer)
+            throws BankOperationException {
+        outTransfer.doOperation(sourceBankAccount);
+    }
 }
