c969f11ba4d222432a265832e510776647f9c0e5
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-12-11 23:14:14 +0100

    Implemented transfer bank operation

---
+++ src/main/java/pl/poznan/put/bsr/bank/models/bankOperations/Transfer.java
@@ -0,0 +1,57 @@
+package pl.poznan.put.bsr.bank.models.bankOperations;
+
+import org.mongodb.morphia.Datastore;
+import pl.poznan.put.bsr.bank.exceptions.BankOperationException;
+import pl.poznan.put.bsr.bank.models.BankAccount;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author Kamil Walkowiak
+ */
+public class Transfer extends BankOperation {
+    public enum TransferDirection {
+        IN, OUT
+    }
+
+    @NotNull
+    private String sourceAccountNo;
+    @NotNull
+    private TransferDirection direction;
+
+    public Transfer() {
+    }
+
+    Transfer(String title, double amount, String sourceAccountNo, String targetAccountNo, TransferDirection direction) {
+        super(title, amount, targetAccountNo);
+        this.sourceAccountNo = sourceAccountNo;
+        this.direction = direction;
+    }
+
+    public String getSourceAccountNo() {
+        return sourceAccountNo;
+    }
+
+    public void setSourceAccountNo(String sourceAccountNo) {
+        this.sourceAccountNo = sourceAccountNo;
+    }
+
+    public TransferDirection getDirection() {
+        return direction;
+    }
+
+    public void setDirection(TransferDirection direction) {
+        this.direction = direction;
+    }
+
+    @Override
+    protected void execute(BankAccount bankAccount) throws BankOperationException {
+        if(direction == TransferDirection.IN) {
+            bankAccount.setBalance(bankAccount.getBalance() + amount);
+        } else {
+            bankAccount.setBalance(bankAccount.getBalance() - amount);
+        }
+        this.balanceAfter = bankAccount.getBalance();
+        bankAccount.addBankOperation(this);
+    }
+}
