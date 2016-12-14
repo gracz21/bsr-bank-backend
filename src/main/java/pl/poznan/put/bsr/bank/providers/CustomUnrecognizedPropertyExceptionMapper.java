515c11c6f5727ff48afa8e5ca9cacb7315be86d1
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-12-14 17:22:12 +0100

    Added missing import

--- src/main/java/pl/poznan/put/bsr/bank/providers/CustomUnrecognizedPropertyExceptionMapper.java
+++ src/main/java/pl/poznan/put/bsr/bank/providers/CustomUnrecognizedPropertyExceptionMapper.java
@@ -4,13 +4,14 @@ import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 /**
  * @author Kamil Walkowiak
  */
 @Provider
-public class CustomUnrecognizedPropertyExceptionMapper implements javax.ws.rs.ext.ExceptionMapper<UnrecognizedPropertyException> {
+public class CustomUnrecognizedPropertyExceptionMapper implements ExceptionMapper<UnrecognizedPropertyException> {
     @Override
     public Response toResponse(UnrecognizedPropertyException e) {
         return Response.status(Response.Status.BAD_REQUEST)
