2c16703c6350a4e754771e805338108f19102ffd
Kamil Walkowiak <kamil.walek21@gmail.com>
2017-01-17 23:38:41 +0100

    Removed bad import in custom resource config class

--- src/main/java/pl/poznan/put/bsr/bank/CustomResourceConfig.java
+++ src/main/java/pl/poznan/put/bsr/bank/CustomResourceConfig.java
@@ -3,7 +3,6 @@ package pl.poznan.put.bsr.bank;
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import org.glassfish.jersey.server.ResourceConfig;
 import pl.poznan.put.bsr.bank.providers.CustomJsonMappingExceptionMapper;
-import pl.poznan.put.bsr.bank.providers.CustomJsonParseExceptionMapper;
 import pl.poznan.put.bsr.bank.providers.CustomJsonProcessingExceptionMapper;
 import pl.poznan.put.bsr.bank.providers.CustomUnrecognizedPropertyExceptionMapper;
 import pl.poznan.put.bsr.bank.utils.BasicAuthFilterUtil;
